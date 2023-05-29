4641baa9564b29eac62ec47453b71fcaf744aafb
xingyijun <xing_yijun@venusgroup.com.cn>
2023-05-29 17:37:41 +0800

    极简版架构搭建

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <my-header></my-header>
+    <main>
+      <slot></slot>
+    </main>
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+main {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
